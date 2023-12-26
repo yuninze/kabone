d27338772513437ce79dadfdb7b43af76f683027
yinze <gajeongsick@gmail.com>
2023-12-26 21:04:59 +0900

    #

--- ss.py
+++ ss.py
@@ -3,14 +3,16 @@ import pandas as pd
 from datetime import datetime
 import gspread
 
-dtformat="%yyyy-%m-%d %H:%M:%S"
-ln="-"*10
+datetimeFormat="%Y-%m-%d %H:%M:%S"
+padder=". . ."
 
 def ima():
-    ima=datetime.now().strftime(dtformat)
+    ima=datetime.now().strftime(datetimeFormat)
     return (ima,ima.replace(":",""))
 
 class Gs:
+    # gs, sheet, worksheet
+
     def __init__(self,key):
         self.key=key
         self.gs=gspread.service_account(self.key)
@@ -25,16 +27,29 @@ class Gs:
     def select(self,worksheet):
         self.worksheet=self.sheet.worksheet(worksheet)
     
+    def get(self,area=None):
+        if area is None:
+            return self.worksheet.get_all_values()
+        else:
+            return self.worksheet.get_all_records(area)
+    
+    def update(self,data):
+        data=data.fillna(np.nan).replace([np.nan],[None])
+        data=[data.columns.tolist()]+data.values.tolist()
+        final=self.worksheet.update(data)
+        self.worksheet.format("1:1",{"textFormat":{"bold":True}})
+        return final
+    
     def status(self):
         return self.sheet.list_permissions()
 
     def give(self,mail,perm_type="user",role="reader",notify=False):
         self.sheet.share(mail,role=role,perm_type=perm_type,notify=notify)
 
-    def add(self,title,size=(1000,10)):
-        if not isinstance(title,str):
-            raise TypeError(f"title accepts str, but input is {type(title)}")
-        _title=f"{datetime.now().strftime(dtformat)}_{title}"
+    def add(self,title=None,size=(1000,10)):
+        if title is None:
+            title="_"
+        _title=f"{datetime.now().strftime(datetimeFormat)}_{title}"
         self.sheet.add_worksheet(_title,rows=size[0],cols=size[1])
         _wks=self.sheet.worksheets()
         _wks.reverse()
@@ -77,13 +92,18 @@ def sookso(sheet="15n--rM5LdNbiPktYXXN2VAEC4cIw9xb-xZ8SXeM8bFg",worksheet="sooks
     
     return data,ima()[1]
 
-data=basis()[0].set_index("idx")
-this=sookso()[0].set_index("idx")
+def exec():
+    data=basis()[0].set_index("idx")
+    this=sookso()[0].set_index("idx")
 
-if not this.index.isin(data.index).all():
-    raise IndexError(f"Unmatched indices {set(this.index)-set(data.index)} in incoming data")
+    if not this.index.isin(data.index).all():
+        raise IndexError(f"Unmatched indices {set(this.index)-set(data.index)} in incoming data")
 
-data.to_csv("d:/dg.data.csv",encoding="utf-8")
-this.to_csv("d:/dg.this.csv",encoding="utf-8")
+    data.to_csv("d:/dg.data.csv",encoding="utf-8")
+    this.to_csv("d:/dg.this.csv",encoding="utf-8")
 
 # basis.combine_first(sookso)
+
+# gs.open("1y72iPCNF-jeQsg6NS-W83IHdBMy08Ega_CjCE5Rv2Dc")
+# gs.select("data")
+# gs.update(<pd.DataFrame>)
